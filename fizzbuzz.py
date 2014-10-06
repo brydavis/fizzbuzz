44301669aed5fccb49da1164cd72d6d759cbfc2e
Bryan Davis <bryan@neoty.pe>
2014-10-05 23:20:23 -0700

    Create fizzbuzz.py
    python version

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def fizzbuzz(top):
+    for i in range(1,top + 1):
+        if (i % 5 == 0) and (i % 3 == 0):
+            print 'FizzBuzz'
+        elif (i % 3 == 0):
+            print 'Fizz'
+        elif (i % 5 == 0):
+            print 'Buzz'
+        else:
+            print i
+    return

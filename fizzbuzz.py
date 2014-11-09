b91f47830c806fdad675f6c30e58f360424b5562
Bryan Davis <deluxicon@users.noreply.github.com>
2014-11-09 02:19:35 -0800

    merge python examples

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -14,4 +14,18 @@ def fizzbuzz(top):
             print i
     return
 
+def fzzybzzy(top):
+    
+    for i in range(1,top + 1):
+        s = ''
+        
+        s += 'Fizz' if (i % 3 == 0) else ''
+        s += 'Buzz' if (i % 5 == 0) else ''
+        
+        print s or i
+    
+    return
+
 fizzbuzz(argv[1])
+fzzybzzy(argv[1])
+

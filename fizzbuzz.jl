939d3accf3fccb6ad00af12f1cd499dbbf6ffce1
Bryan Davis <deluxicon@users.noreply.github.com>
2014-11-09 02:24:25 -0800

    Create fizzbuzz.jl

---
+++ fizzbuzz.jl
@@ -0,0 +1,12 @@
+#!/usr/bin/env julia 
+
+function fizzbuzz(top)  
+	for i in range(1,int(top))
+		s = ""
+		s *= (i % 3 == 0)? "Fizz" : ""
+		s *= (i % 5 == 0)? "Buzz" : ""
+		s == ""? println(i): println(s)
+	end
+end
+
+fizzbuzz(ARGS[1])
